dd780cb3a680ad815685c695067878241c1e8b77
GugaSantos <guga.santos.guga@gmail.com>
2023-03-24 08:47:00 -0300

    feat: create api request

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,12 @@
-const Spotify = require('../../data/spotify.json')
-
 class API {
   async getSpotify() {
-    const response = await (await fetch(Spotify)).json();
-    return response;
+    const response = await fetch('http://localhost:3001/spotify');
+    const data = await response.json();
+    return data;
+  }
+  async getYoutube() {
+    const response = await fetch('http://localhost:3001/youtube');
+    const data = await response.json();
+    return data;
   }
 }
-
-const api = new API()
-console.log(api.getSpotify())
