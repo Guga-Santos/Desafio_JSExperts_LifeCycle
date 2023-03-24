90409a4a542f5cb75baa1d7b469067b62e371e48
GugaSantos <guga.santos.guga@gmail.com>
2023-03-24 10:45:00 -0300

    test: service test structure

---
+++ test/unit/service.test.js
@@ -0,0 +1,24 @@
+const { describe, it } =  require('mocha');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const API = require('./../../src/api');
+const repository = require('./../../src/repository')/
+
+describe('Service Suite Tests', () => {
+  describe('Music Service', () => {
+    it('Ensure it returns data', () => {
+
+    })
+  })
+  describe('Youtube Service', () => {
+    it('Ensure it returns data', () => {
+
+    })
+  })
+  describe('Spotify Service', () => {
+    it('Ensure it returns data', () => {
+
+    })
+  })
+}) 
