69fdc8ce63895a0634c8c4e3d606ef45168b1d1a
GugaSantos <guga.santos.guga@gmail.com>
2023-03-24 07:33:21 -0300

    feat: create routes

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+const routes = {
+  '/:get': (req, res) => {
+    res.writeHead(200);
+    res.write('Bem-vindo à Página Inicial');
+    return res.end()
+  },
+  '/youtube:get': (req, res) => {
+    res.writeHead(200);
+    res.write('Página da playslist do Youtube');
+    return res.end();
+  },
+  '/spotify:get': (req, res) => {
+    res.writeHead(200);
+    res.write('Página da playslist do Spotify');
+    return res.end();
+  },
+  default: (req, res) => {
+    res.writeHead(404);
+    res.write('Bad Request: Page Not Found!');
+    return res.end();
+  }
+}
