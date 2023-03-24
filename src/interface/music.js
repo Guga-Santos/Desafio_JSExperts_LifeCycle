7cb68f0703d711141b192ac821866e999953034b
GugaSantos <guga.santos.guga@gmail.com>
2023-03-24 11:31:07 -0300

    feat: Music interface

---
+++ src/interface/music.js
@@ -0,0 +1,8 @@
+export default class Music {
+  constructor({artist_name, album_name, music_name, duration }) {
+      this.artist_name = artist_name;
+      this.album_name = album_name;
+      this.music_name = music_name;
+      this.duration = duration;
+  }
+}
