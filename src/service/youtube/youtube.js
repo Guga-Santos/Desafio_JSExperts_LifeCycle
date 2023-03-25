e16653473910e0cc794f14ae8147aaf8612513fa
GugaSantos <guga.santos.guga@gmail.com>
2023-03-25 15:49:06 -0300

    feat: youtube service

--- src/service/youtube/youtube.js
+++ src/service/youtube/youtube.js
@@ -1,7 +1,17 @@
 const Music = require("../music/music");
 
 class YoutubeMusic extends Music {
+  constructor({ ...tracks }) {
+    super((tracks));
+  }
 
+  toString() {
+    return `${this.music_name} - ${this.album_name} - ${this.artist_name}`
+  }
+
+  valueOf() {
+    return this.duration;
+  }
 }
 
 module.exports = YoutubeMusic;
