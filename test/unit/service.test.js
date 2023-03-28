2aafd0fcdf564e350484f02fd2d8242daaf5a009
GugaSantos <guga.santos.guga@gmail.com>
2023-03-28 14:49:18 -0300

    test: service test imports

--- test/unit/service.test.js
+++ test/unit/service.test.js
@@ -2,10 +2,21 @@ const { describe, it } =  require('mocha');
 const sinon = require('sinon');
 const { expect } = require('chai');
 
+const Music = require('./../../src/service/music/music');
+const Youtube = require('./../../src/service/youtube/youtube');
+const Spotify = require('./../../src/service/spotify/spotify');
+
+
 const API = require('./../../src/api');
-const repository = require('./../../src/repositories')/
+const Repository = require('./../../src/repositories')/
 
 describe('Service Suite Tests', () => {
+  let api = {};
+  let repository = {};
+  let music = {};
+  let spotify = {};
+  let youtube = {};
+  
   describe('Music Service', () => {
     it('Ensure it returns data', () => {
 
