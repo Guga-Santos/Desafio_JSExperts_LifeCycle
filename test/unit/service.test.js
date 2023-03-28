8f2165a4fcf6caa6f09dbe580daa33c1fc6ae992
GugaSantos <guga.santos.guga@gmail.com>
2023-03-28 15:25:14 -0300

    test: create a new service

--- test/unit/service.test.js
+++ test/unit/service.test.js
@@ -15,6 +15,20 @@ const SpotifyRepository = require('./../../src/service/spotify/repository');
 const API = require('./../../src/api');
 const Repository = require('./../../src/repositories')
 
+
+const youtubeMock = {
+  repository: [
+    {
+      artist_name: 'Avicii',
+      album_name: 'Stories',
+      music_name: 'Waiting For Love',
+      duration: 230613,
+      image: 'https://i.scdn.co/image/ab67616d0000b2735393c5d3cac806092a9bc468',
+      music_preview: 'https://p.scdn.co/mp3-preview/407e05795004323b971853a57c8a141bb164645b?cid=d8a5ed958d274c2e8ee717e6a4b0971d'
+    }
+  ]
+}
+
 describe('Service Suite Tests', () => {
   let api = {};
   let repository = {};
@@ -22,8 +36,18 @@ describe('Service Suite Tests', () => {
   let spotify = {};
   let youtube = {};
 
+  let sandbox = {};
+
   before(() => {
     api = new API();
+  })
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox()
+  })
+
+  afterEach(() => {
+    sandbox.restore();
   })
   // describe('Music Service', () => {
   //   it('Ensure it returns data', () => {
@@ -33,7 +57,10 @@ describe('Service Suite Tests', () => {
   describe('Youtube Service', () => { 
     before(async () => {
       repository = new YoutubeRepository({ api })
-      youtube = new Youtube({ repository });
+      const data = await repository.getAll();
+      // const test = data.map((item) => new Youtube( item ))
+
+      console.log(data)
     })
     it('Ensure it returns data', () => {
 
